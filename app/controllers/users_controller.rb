755b94400f8130e5a23bc87faa83edc49d26f844
zaid bargothy <zaidboabo@gmail.com>
2020-07-05 18:02:02 +0300

    createUser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
- def new
+ 
+    def new
   @user = User.new
+  end
+  
+  def create
+  @user = User.new(user_params)
+  if @user.save
+    flash[:notice] = "Welcome to my hubmle website"
+    redirect_to articles_path
+  end
+end
+  
+  private 
+
+  def user_params
+  params.require(:user).permit(:username, :email, :password)
   end
 
  end
